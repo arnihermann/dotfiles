ad73366d1631c91ab472ccf105d3474c148d2064
Árni Hermann Reynisson <arnihr@gmail.com>
2014-01-19 23:14:53 +0000

    Fix broken zsh autocomplete

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -1,13 +1,8 @@
-if [[ -n $SSH_CONNECTION ]]; then
-  export PS1='%m:%3~$(git_info_for_prompt)%# '
-else
-  export PS1='%3~$(git_info_for_prompt)%# '
-fi
-
 export LSCOLORS="exfxcxdxbxegedabagacad"
 export CLICOLOR=true
 export DISABLE_LS_COLORS=true
 export DISABLE_AUTO_UPDATE=true
+export LANG=is_IS.UTF-8
 
 fpath=($DOTFILES/functions $fpath)
 
