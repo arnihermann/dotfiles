ba518447d6d9b2d9f35784d01fabb87fe37b4b95
Árni Hermann Reynisson <arnihr@gmail.com>
2014-01-20 10:24:18 +0000

    Add postgres to path

--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,5 @@
 export PATH="./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$DOTFILES/bin:$PATH"
 
+export PATH="/Applications/Postgres93.app/Contents/MacOS/bin:$PATH"
+
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
