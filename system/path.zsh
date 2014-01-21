60ff6db0269493c87392f1ace3806ccb4f1a1360
Árni Hermann Reynisson <arnihr@gmail.com>
2014-01-21 16:44:18 +0000

    Change PATH variables

--- system/path.zsh
+++ system/path.zsh
@@ -2,4 +2,6 @@ export PATH="./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:
 
 export PATH="/Applications/Postgres93.app/Contents/MacOS/bin:$PATH"
 
+export PATH="/Users/arnihermann/bin:$PATH"
+
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
