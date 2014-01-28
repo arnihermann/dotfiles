8cd0cdb58f9ceed9dac80d0e1c29637b02a8a528
Árni Hermann Reynisson <arnihr@gmail.com>
2014-01-28 16:32:04 +0000

    Add local node_modules directories into path

--- system/path.zsh
+++ system/path.zsh
@@ -4,4 +4,6 @@ export PATH="/Applications/Postgres93.app/Contents/MacOS/bin:$PATH"
 
 export PATH="/Users/arnihermann/bin:$PATH"
 
+export PATH="node_modules/.bin:$PATH"
+
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
